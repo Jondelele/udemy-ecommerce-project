6526bb11092a446d150899b0a1e798f030a68d68
Joona Prehti <joona.prehti@gmail.com>
2020-05-24 20:10:55 +0300

    Toteutin category TS filun, next service

--- 03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductCategory} from "../../common/product-category";
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product-category-menu',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCategoryMenuComponent implements OnInit {
 
-  constructor() { }
+  productCategories: ProductCategory[];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProductCategories();
   }
 
+  listProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      // Eli kun data tuote kategorioista saahaan takaisin niin se laitetaan this.productCategories member variin
+      data => {
+        console.log('Product categories=' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    )
+  }
 }
