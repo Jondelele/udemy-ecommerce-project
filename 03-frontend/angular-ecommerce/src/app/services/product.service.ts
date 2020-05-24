09780c1b50daecd8202e9da1a6676488d7a829e2
Joona Prehti <joona.prehti@gmail.com>
2020-05-24 13:39:51 +0300

    Added few comments

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -19,12 +19,17 @@ export class ProductService {
   // ja juuri ennen varsinaista koodia ja ekaa aaltosuljetta. Service laitetaan palauttamaan Observable<Product[]>
   // objecti, jonka avulla spring backarilta saatu data mappaantuu TS koodin puolella Product TS luokan mukaisesti.
   getProductList(): Observable<Product[]> {
+    // < ja > merkkien välissä on GetResponse interface, koska se maarittaa minkäläiseksi haettu JSON data castataan.
+    // Paramteriksi ei tarvita mitään muuta kuin URL, josta JSON product data haetaan. Palautunut vastaus ohjataan
+    // pipellä map funktiolle, joka hakee tuotteet responsesta _embedded.products attribuutista.
+    // _embedded on siis olio, jonka sisällä on products lista, jonka sisällä siis productit tuotteet ovat!!!
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 }
 
+// interface GetResponse määrittää mihin muotoon REST API:n palauttama palautus castataan.
 interface GetResponse {
   _embedded: {
     products: Product[];
