cbf4f7d7f5df7e38262c562799c2296620e3b998
Joona Prehti <joona.prehti@gmail.com>
2020-05-24 13:44:54 +0300

    Added few comments2

--- 03-frontend/angular-ecommerce/src/app/common/product.ts
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,3 +1,9 @@
+// Luokkaa Product määrittää muodon tuote datalle frontissa
+// Käyetään esimerkiksi product-list.component.ts filussa luokassa ProductListComponent luodessa
+// jäsenmuuttujaa: products: Product[]; johon sitten tallennetaan backarilta servicen noutama
+// lista tuotteista
+// Product luokkaa käyettään myös kun servicen hakema JSON lista castataan Product muotoon
+// interfacessa GetResponse
 export class Product {
   sku: string;
   name: string;
