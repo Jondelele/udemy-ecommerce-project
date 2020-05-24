dd9397f2b4079ef6f17190c90eacb916927dbb78
Joona Prehti <joona.prehti@gmail.com>
2020-05-24 18:13:48 +0300

    Made ProductService to use URL that has dynamic params

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -19,13 +19,14 @@ export class ProductService {
   // ja juuri ennen varsinaista koodia ja ekaa aaltosuljetta. Service laitetaan palauttamaan Observable<Product[]>
   // objecti, jonka avulla spring backarilta saatu data mappaantuu TS koodin puolella Product TS luokan mukaisesti.
   getProductList(theCategoryId: number): Observable<Product[]> {
-
+    // Luodaan uusi URL category id:n mukaan
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
     // < ja > merkkien välissä on GetResponse interface, koska se maarittaa minkäläiseksi haettu JSON data castataan.
     // Paramteriksi ei tarvita mitään muuta kuin URL, josta JSON product data haetaan. Palautunut vastaus ohjataan
     // pipellä map funktiolle, joka hakee tuotteet responsesta _embedded.products attribuutista.
     // _embedded on siis olio, jonka sisällä on products lista, jonka sisällä siis productit tuotteet ovat!!!
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
