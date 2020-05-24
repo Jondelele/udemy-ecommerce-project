b66d87df967fe514114ba3a0b52bd51b2b377871
Joona Prehti <joona.prehti@gmail.com>
2020-05-24 10:54:22 +0300

    Implement service for product

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Product } from "../common/product";
+import { map } from "rxjs/operators";
 
+// @Injectable dekoraattori ProductService luokan päällä tarkoittaa että tämä service kyetään injektoimaan
+// komponenttiin root eli kaikkiin komponetteihin koska root on se pää komponentti
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8081/api/product';
+
+  constructor(private httpClient: HttpClient) { }
+
+  // TypeScriptissä todella hassu paluuarvon paikka koska se tulee paramien jälkeen
+  // ja juuri ennen varsinaista koodia ja ekaa aaltosuljetta. Service laitetaan palauttamaan Observable<Product[]>
+  // objecti, jonka avulla spring backarilta saatu data mappaantuu TS koodin puolella Product TS luokan mukaisesti.
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
